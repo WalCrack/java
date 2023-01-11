9d465f38a12f85ad117464f013a333cfb334baa7
WalCrack <1@gmail.com>
2023-01-11 23:04:41 +0500

    lr7

--- src/lr7/example1/example1.java
+++ src/lr7/example1/example1.java
@@ -5,11 +5,9 @@ public class example1 {
         superClass example1 = new superClass("Конструктор суперкласса");
         String className = example1.toString();
         System.out.println(className);
-
         podClass example1_1 = new podClass("Конструктор подкласса");
         String subClassName = example1_1.toString();
         System.out.println(subClassName);
-
         podClass example1_2 = new podClass("Конструктор подкласса", "это 2ой параметр");
         String subClassName2 = example1_2.toString();
         System.out.println(subClassName2);
